843bbfd8a78c5dce0b3d295b7e966dbdfea188bd
marcosig1 <msiguencia@jungroup.com>
2024-07-17 10:45:45 -0400

    removed moduloing the random number

--- generateappuserid.sh
+++ generateappuserid.sh
@@ -5,8 +5,9 @@ num_of_ids=$2
 for (( i=0; i < num_of_ids; i++));
 do
 	uid=$(uuidgen | awk '{print tolower($0)}')
-	printf "%d.%s\n" "$(($RANDOM%256))" "$uid">> $1
+	printf "%d.%s\n" "$RANDOM" "$uid">> $1
 	echo $i
+	#disregard this
 	#printf "%d\r" "$(($i/$num_of_ids))"
 	#sleep 0.1
 done
